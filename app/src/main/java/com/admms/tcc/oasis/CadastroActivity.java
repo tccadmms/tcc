72454d5e875264653400e7b6ee9ce43f05ae771c
BacMa <marcobacelar@gmail.com>
2016-07-24 17:02:34 -0300

    Alterações Tela Inicial, Manifest com debugger, inserção de logo no projeto.

---
+++ app/src/main/java/com/admms/tcc/oasis/CadastroActivity.java
@@ -0,0 +1,35 @@
+package com.admms.tcc.oasis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+
+        Button botaoSalvar = (Button) findViewById(R.id.cadastro_btnSalvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(CadastroActivity.this, "Cadastro realizado com sucesso!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_cadastro, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}

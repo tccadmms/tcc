c5aa3c77ea3ea977f181648d3b6c30c5da348b1e
BacMa <marcobacelar@gmail.com>
2016-07-25 13:03:52 -0300

    alterações de layout, disable de rotação da tela inicial.

--- app/src/main/java/com/admms/tcc/oasis/MainActivity.java
+++ app/src/main/java/com/admms/tcc/oasis/MainActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 

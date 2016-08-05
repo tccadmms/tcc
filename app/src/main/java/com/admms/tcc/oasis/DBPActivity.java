5e7805c4593bc6e42a2b43fd8c77e22b9de588a3
BacMa <marcobacelar@gmail.com>
2016-08-04 22:26:06 -0300

    Alteração de animação tela splash e menu inicial.

--- app/src/main/java/com/admms/tcc/oasis/DBPActivity.java
+++ app/src/main/java/com/admms/tcc/oasis/DBPActivity.java
@@ -1,44 +1,127 @@
 package com.admms.tcc.oasis;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.PixelFormat;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-public class DBPActivity extends AppCompatActivity {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
+public class DBPActivity extends Activity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+
+    /**
+     * Called when the activity is first created.
+     */
+    Thread splashTread;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dbp);
+        StartAnimations();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
 
-        final ImageView abertura = (ImageView) findViewById(R.id.dbp_abertura);
-        final Animation dbp_animacao = AnimationUtils.loadAnimation(getBaseContext(),R.anim.dbp_apresentacao);
-        final Animation dbp_animacao2 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade_out);
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        LinearLayout l = (LinearLayout) findViewById(R.id.lin_lay);
+        l.clearAnimation();
+        l.startAnimation(anim);
 
+        anim = AnimationUtils.loadAnimation(this, R.anim.dbp_apresentacao);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.splash);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
 
-        abertura.startAnimation(dbp_animacao);
-        dbp_animacao.setAnimationListener(new Animation.AnimationListener() {
+        splashTread = new Thread() {
             @Override
-            public void onAnimationStart(Animation animation) {
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 3500) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(DBPActivity.this,
+                            MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    DBPActivity.this.finish();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    DBPActivity.this.finish();
+                }
 
             }
+        };
+        splashTread.start();
 
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                abertura.startAnimation(dbp_animacao2);
-                finish();
-                Intent vaiPraMain = new Intent(DBPActivity.this,MainActivity.class);
-                startActivity(vaiPraMain);
+    }
 
-            }
+    @Override
+    public void onStart() {
+        super.onStart();
 
-            @Override
-            public void onAnimationRepeat(Animation animation) {
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "DBP Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.admms.tcc.oasis/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
 
-            }
-        });
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "DBP Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.admms.tcc.oasis/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }

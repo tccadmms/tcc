9583196f858a148a65577ffe5eaf3e5ff85e279d
thealphazen <mazen.asefali@gmail.com>
2016-08-07 16:40:43 -0300

    Ativação da interface view/banco de dados

--- app/src/main/java/com/admms/tcc/oasis/CadastroActivity.java
+++ app/src/main/java/com/admms/tcc/oasis/CadastroActivity.java
@@ -7,13 +7,22 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.admms.tcc.oasis.dao.DatabaseHelper;
+import com.admms.tcc.oasis.entity.Estabelecimento;
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+import com.j256.ormlite.dao.RuntimeExceptionDao;
+
 public class CadastroActivity extends AppCompatActivity {
 
+    DatabaseHelper databaseHelper;
 
+    private EditText cnpj, razaoSocial, email, telefone, cidade, cep, ramoAtividade, proprietario;
+    private Spinner estado, legislacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +36,35 @@ public class CadastroActivity extends AppCompatActivity {
         estados.setAdapter(adapterEstados);
         legislacoes.setAdapter(adapterLegislacoes);
 
+        //Preparando o db para salvar onClick
+        databaseHelper = OpenHelperManager.getHelper(this, DatabaseHelper.class);
+        final RuntimeExceptionDao<Estabelecimento, Integer> estabelecimentoDao = databaseHelper.getEstabelecimentoRuntimeExceptionDao();
+
         ImageButton botaoSalvar = (ImageButton) findViewById(R.id.cadastro_btnSalvar);
         botaoSalvar.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
+                cnpj = (EditText) findViewById(R.id.cadastro_cnpj);
+                razaoSocial = (EditText) findViewById(R.id.cadastro_razaoSocial);
+                email = (EditText) findViewById(R.id.cadastro_email);
+                telefone = (EditText) findViewById(R.id.cadastro_telefone);
+                cidade = (EditText) findViewById(R.id.cadastro_cidade);
+                estado = (Spinner) findViewById(R.id.cadastro_estados);
+                cep = (EditText) findViewById(R.id.cadastro_cep);
+                ramoAtividade = (EditText) findViewById(R.id.cadastro_ramoDeAtividade);
+                proprietario = (EditText) findViewById(R.id.cadastro_proprietarioDoEstabelecimento);
+                legislacao = (Spinner) findViewById(R.id.cadastro_legislacao);
 
+                estabelecimentoDao.create(new Estabelecimento(String.valueOf(razaoSocial.getText()),
+                                                              String.valueOf(email.getText()),
+                                                              String.valueOf(cnpj.getText()),
+                                                              String.valueOf(cep.getText()),
+                                                              String.valueOf(cidade.getText()),
+                                                              String.valueOf(estado.getSelectedItem().toString()),
+                                                              String.valueOf(ramoAtividade.getText()),
+                                                              String.valueOf(proprietario.getText()),
+                                                              String.valueOf(legislacao.getSelectedItem().toString())));
                 Toast.makeText(CadastroActivity.this, "Cadastro realizado com sucesso!", Toast.LENGTH_SHORT).show();
                 final Intent intentVaiProItens = new Intent(CadastroActivity.this, ItensDeAvaliacaoActivity.class);
                 Thread thread = new Thread(){

8642efa12fa38565edcc2688686835b00d2d1889
BacMa <marcobacelar@gmail.com>
2016-09-21 13:28:38 -0300

    formulários PRT 2619/2011 ok.

---
+++ app/src/main/java/com/admms/tcc/oasis/controller/prt2619/Prt2619_ResiduosActivity.java
@@ -0,0 +1,246 @@
+package com.admms.tcc.oasis.controller.prt2619;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.admms.tcc.oasis.R;
+
+public class Prt2619_ResiduosActivity extends Activity {
+
+    private static final int REQUEST_IMAGE_PICTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prt2619__residuos);
+
+        RadioGroup r_p1 = (RadioGroup) findViewById(R.id.residuos_p1);
+
+
+        r_p1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton na_p1 = (RadioButton) findViewById(R.id.residuos_p1_na);
+                RadioButton ad_p1 = (RadioButton) findViewById(R.id.residuos_p1_ad);
+                RadioButton in_p1 = (RadioButton) findViewById(R.id.residuos_p1_in);
+
+                ImageButton foto_p1 = (ImageButton) findViewById(R.id.residuos_foto_p1);
+                ImageButton descricao_p1 = (ImageButton) findViewById(R.id.residuos_descricao_p1);
+
+
+                if (in_p1.isChecked()) {
+                    foto_p1.setVisibility(View.VISIBLE);
+                    descricao_p1.setVisibility(View.VISIBLE);
+                    foto_p1.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            tirarFotoIntent();
+                        }
+                    });
+                    descricao_p1.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            mostraJanelaDescricao();
+                        }
+                    });
+
+                } else if (na_p1.isChecked()) {
+                    foto_p1.setVisibility(View.INVISIBLE);
+                    descricao_p1.setVisibility(View.INVISIBLE);
+                } else {
+                    foto_p1.setVisibility(View.INVISIBLE);
+                    descricao_p1.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        RadioGroup r_p2 = (RadioGroup) findViewById(R.id.residuos_p2);
+
+
+        r_p2.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton na_p2 = (RadioButton) findViewById(R.id.residuos_p2_na);
+                RadioButton ad_p2 = (RadioButton) findViewById(R.id.residuos_p2_ad);
+                RadioButton in_p2 = (RadioButton) findViewById(R.id.residuos_p2_in);
+
+                ImageButton foto_p2 = (ImageButton) findViewById(R.id.residuos_foto_p2);
+                ImageButton descricao_p2 = (ImageButton) findViewById(R.id.residuos_descricao_p2);
+
+
+                if (in_p2.isChecked()) {
+                    foto_p2.setVisibility(View.VISIBLE);
+                    descricao_p2.setVisibility(View.VISIBLE);
+                    foto_p2.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            tirarFotoIntent();
+                        }
+                    });
+                    descricao_p2.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            mostraJanelaDescricao();
+                        }
+                    });
+
+                } else if (na_p2.isChecked()) {
+                    foto_p2.setVisibility(View.INVISIBLE);
+                    descricao_p2.setVisibility(View.INVISIBLE);
+                } else {
+                    foto_p2.setVisibility(View.INVISIBLE);
+                    descricao_p2.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        RadioGroup r_p3 = (RadioGroup) findViewById(R.id.residuos_p3);
+
+
+        r_p3.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton na_p3 = (RadioButton) findViewById(R.id.residuos_p3_na);
+                RadioButton ad_p3 = (RadioButton) findViewById(R.id.residuos_p3_ad);
+                RadioButton in_p3 = (RadioButton) findViewById(R.id.residuos_p3_in);
+
+                ImageButton foto_p3 = (ImageButton) findViewById(R.id.residuos_foto_p3);
+                ImageButton descricao_p3 = (ImageButton) findViewById(R.id.residuos_descricao_p3);
+
+
+                if (in_p3.isChecked()) {
+                    foto_p3.setVisibility(View.VISIBLE);
+                    descricao_p3.setVisibility(View.VISIBLE);
+                    foto_p3.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            tirarFotoIntent();
+                        }
+                    });
+                    descricao_p3.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            mostraJanelaDescricao();
+                        }
+                    });
+
+                } else if (na_p3.isChecked()) {
+                    foto_p3.setVisibility(View.INVISIBLE);
+                    descricao_p3.setVisibility(View.INVISIBLE);
+                } else {
+                    foto_p3.setVisibility(View.INVISIBLE);
+                    descricao_p3.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        RadioGroup r_p4 = (RadioGroup) findViewById(R.id.residuos_p4);
+
+
+        r_p4.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton na_p4 = (RadioButton) findViewById(R.id.residuos_p4_na);
+                RadioButton ad_p4 = (RadioButton) findViewById(R.id.residuos_p4_ad);
+                RadioButton in_p4 = (RadioButton) findViewById(R.id.residuos_p4_in);
+
+                ImageButton foto_p4 = (ImageButton) findViewById(R.id.residuos_foto_p4);
+                ImageButton descricao_p4 = (ImageButton) findViewById(R.id.residuos_descricao_p4);
+
+
+                if (in_p4.isChecked()) {
+                    foto_p4.setVisibility(View.VISIBLE);
+                    descricao_p4.setVisibility(View.VISIBLE);
+                    foto_p4.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            tirarFotoIntent();
+                        }
+                    });
+                    descricao_p4.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            mostraJanelaDescricao();
+                        }
+                    });
+
+                } else if (na_p4.isChecked()) {
+                    foto_p4.setVisibility(View.INVISIBLE);
+                    descricao_p4.setVisibility(View.INVISIBLE);
+                } else {
+                    foto_p4.setVisibility(View.INVISIBLE);
+                    descricao_p4.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+
+        ImageButton residuosSalvar = (ImageButton) findViewById(R.id.residuos_btnSalvar);
+        residuosSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentVaiProResiduos = new Intent(Prt2619_ResiduosActivity.this, Prt2619Activity.class);
+                startActivity(intentVaiProResiduos);
+            }
+        });
+
+
+    }
+
+    private void tirarFotoIntent() {
+        Intent vaiPraCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (vaiPraCamera.resolveActivity(getPackageManager())!= null) {
+            startActivityForResult(vaiPraCamera, REQUEST_IMAGE_PICTURE);
+        }
+    }
+
+
+
+    protected void mostraJanelaDescricao() {
+
+
+        LayoutInflater layoutInflater = LayoutInflater.from(Prt2619_ResiduosActivity.this);
+        View promptView = layoutInflater.inflate(R.layout.activity_pop_up, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Prt2619_ResiduosActivity.this);
+        alertDialogBuilder.setTitle("Descrição");
+        alertDialogBuilder.setIcon(R.drawable.ic_logo);
+        alertDialogBuilder.setView(promptView);
+
+
+        final EditText descricao = (EditText) promptView.findViewById(R.id.descricao);
+
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton("Salvar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+
+                    }
+                })
+                .setNegativeButton("Cancelar",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
+
+}
+
+
+
+
+
---
+++ app/src/main/java/com/admms/tcc/oasis/controller/prt2619/Prt2619_VetoresActivity.java
@@ -0,0 +1,287 @@
+package com.admms.tcc.oasis.controller.prt2619;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.admms.tcc.oasis.R;
+
+public class Prt2619_VetoresActivity extends Activity {
+
+    private static final int REQUEST_IMAGE_PICTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prt2619__vetores);
+
+
+
+        RadioGroup r_p1 = (RadioGroup) findViewById(R.id.vetores_p1);
+
+
+        r_p1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton na_p1 = (RadioButton) findViewById(R.id.vetores_p1_na);
+                RadioButton ad_p1 = (RadioButton) findViewById(R.id.vetores_p1_ad);
+                RadioButton in_p1 = (RadioButton) findViewById(R.id.vetores_p1_in);
+
+                ImageButton foto_p1 = (ImageButton) findViewById(R.id.vetores_foto_p1);
+                ImageButton descricao_p1 = (ImageButton) findViewById(R.id.vetores_descricao_p1);
+
+
+                if (in_p1.isChecked()) {
+                    foto_p1.setVisibility(View.VISIBLE);
+                    descricao_p1.setVisibility(View.VISIBLE);
+                    foto_p1.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            tirarFotoIntent();
+                        }
+                    });
+                    descricao_p1.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            mostraJanelaDescricao();
+                        }
+                    });
+
+                } else if (na_p1.isChecked()) {
+                    foto_p1.setVisibility(View.INVISIBLE);
+                    descricao_p1.setVisibility(View.INVISIBLE);
+                } else {
+                    foto_p1.setVisibility(View.INVISIBLE);
+                    descricao_p1.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        RadioGroup r_p2 = (RadioGroup) findViewById(R.id.vetores_p2);
+
+
+        r_p2.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton na_p2 = (RadioButton) findViewById(R.id.vetores_p2_na);
+                RadioButton ad_p2 = (RadioButton) findViewById(R.id.vetores_p2_ad);
+                RadioButton in_p2 = (RadioButton) findViewById(R.id.vetores_p2_in);
+
+                ImageButton foto_p2 = (ImageButton) findViewById(R.id.vetores_foto_p2);
+                ImageButton descricao_p2 = (ImageButton) findViewById(R.id.vetores_descricao_p2);
+
+
+                if (in_p2.isChecked()) {
+                    foto_p2.setVisibility(View.VISIBLE);
+                    descricao_p2.setVisibility(View.VISIBLE);
+                    foto_p2.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            tirarFotoIntent();
+                        }
+                    });
+                    descricao_p2.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            mostraJanelaDescricao();
+                        }
+                    });
+
+                } else if (na_p2.isChecked()) {
+                    foto_p2.setVisibility(View.INVISIBLE);
+                    descricao_p2.setVisibility(View.INVISIBLE);
+                } else {
+                    foto_p2.setVisibility(View.INVISIBLE);
+                    descricao_p2.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        RadioGroup r_p3 = (RadioGroup) findViewById(R.id.vetores_p3);
+
+
+        r_p3.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton na_p3 = (RadioButton) findViewById(R.id.vetores_p3_na);
+                RadioButton ad_p3 = (RadioButton) findViewById(R.id.vetores_p3_ad);
+                RadioButton in_p3 = (RadioButton) findViewById(R.id.vetores_p3_in);
+
+                ImageButton foto_p3 = (ImageButton) findViewById(R.id.vetores_foto_p3);
+                ImageButton descricao_p3 = (ImageButton) findViewById(R.id.vetores_descricao_p3);
+
+
+                if (in_p3.isChecked()) {
+                    foto_p3.setVisibility(View.VISIBLE);
+                    descricao_p3.setVisibility(View.VISIBLE);
+                    foto_p3.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            tirarFotoIntent();
+                        }
+                    });
+                    descricao_p3.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            mostraJanelaDescricao();
+                        }
+                    });
+
+                } else if (na_p3.isChecked()) {
+                    foto_p3.setVisibility(View.INVISIBLE);
+                    descricao_p3.setVisibility(View.INVISIBLE);
+                } else {
+                    foto_p3.setVisibility(View.INVISIBLE);
+                    descricao_p3.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        RadioGroup r_p4 = (RadioGroup) findViewById(R.id.vetores_p4);
+
+
+        r_p4.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton na_p4 = (RadioButton) findViewById(R.id.vetores_p4_na);
+                RadioButton ad_p4 = (RadioButton) findViewById(R.id.vetores_p4_ad);
+                RadioButton in_p4 = (RadioButton) findViewById(R.id.vetores_p4_in);
+
+                ImageButton foto_p4 = (ImageButton) findViewById(R.id.vetores_foto_p4);
+                ImageButton descricao_p4 = (ImageButton) findViewById(R.id.vetores_descricao_p4);
+
+
+                if (in_p4.isChecked()) {
+                    foto_p4.setVisibility(View.VISIBLE);
+                    descricao_p4.setVisibility(View.VISIBLE);
+                    foto_p4.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            tirarFotoIntent();
+                        }
+                    });
+                    descricao_p4.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            mostraJanelaDescricao();
+                        }
+                    });
+
+                } else if (na_p4.isChecked()) {
+                    foto_p4.setVisibility(View.INVISIBLE);
+                    descricao_p4.setVisibility(View.INVISIBLE);
+                } else {
+                    foto_p4.setVisibility(View.INVISIBLE);
+                    descricao_p4.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        RadioGroup r_p5 = (RadioGroup) findViewById(R.id.vetores_p5);
+
+
+        r_p5.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton na_p5 = (RadioButton) findViewById(R.id.vetores_p5_na);
+                RadioButton ad_p5 = (RadioButton) findViewById(R.id.vetores_p5_ad);
+                RadioButton in_p5 = (RadioButton) findViewById(R.id.vetores_p5_in);
+
+                ImageButton foto_p5 = (ImageButton) findViewById(R.id.vetores_foto_p5);
+                ImageButton descricao_p5 = (ImageButton) findViewById(R.id.vetores_descricao_p5);
+
+
+                if (in_p5.isChecked()) {
+                    foto_p5.setVisibility(View.VISIBLE);
+                    descricao_p5.setVisibility(View.VISIBLE);
+                    foto_p5.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            tirarFotoIntent();
+                        }
+                    });
+                    descricao_p5.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            mostraJanelaDescricao();
+                        }
+                    });
+
+                } else if (na_p5.isChecked()) {
+                    foto_p5.setVisibility(View.INVISIBLE);
+                    descricao_p5.setVisibility(View.INVISIBLE);
+                } else {
+                    foto_p5.setVisibility(View.INVISIBLE);
+                    descricao_p5.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+
+        ImageButton vetoresSalvar = (ImageButton) findViewById(R.id.vetores_btnSalvar);
+        vetoresSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentVaiProVetores = new Intent(Prt2619_VetoresActivity.this, Prt2619Activity.class);
+                startActivity(intentVaiProVetores);
+            }
+        });
+
+
+    }
+
+    private void tirarFotoIntent() {
+        Intent vaiPraCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (vaiPraCamera.resolveActivity(getPackageManager())!= null) {
+            startActivityForResult(vaiPraCamera, REQUEST_IMAGE_PICTURE);
+        }
+    }
+
+
+
+    protected void mostraJanelaDescricao() {
+
+
+        LayoutInflater layoutInflater = LayoutInflater.from(Prt2619_VetoresActivity.this);
+        View promptView = layoutInflater.inflate(R.layout.activity_pop_up, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Prt2619_VetoresActivity.this);
+        alertDialogBuilder.setTitle("Descrição");
+        alertDialogBuilder.setIcon(R.drawable.ic_logo);
+        alertDialogBuilder.setView(promptView);
+
+
+        final EditText descricao = (EditText) promptView.findViewById(R.id.descricao);
+
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton("Salvar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+
+                    }
+                })
+                .setNegativeButton("Cancelar",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
+
+}
+
+
+
+

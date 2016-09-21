a5bd5e59e877248bea89b60c797b190d4e48c627
BacMa <marcobacelar@gmail.com>
2016-09-21 11:51:40 -0300

    formulários PRT 2619/2011 ok.

--- app/src/main/java/com/admms/tcc/oasis/controller/prt2619/Prt2619Activity.java
+++ app/src/main/java/com/admms/tcc/oasis/controller/prt2619/Prt2619Activity.java
@@ -7,17 +7,9 @@ import android.view.View;
 import android.widget.ImageButton;
 
 import com.admms.tcc.oasis.R;
-import com.admms.tcc.oasis.controller.rdc216.Rdc216DocumentacaoActivity;
-import com.admms.tcc.oasis.controller.rdc216.Rdc216EdificacaoActivity;
-import com.admms.tcc.oasis.controller.rdc216.Rdc216IngredientesActivity;
-import com.admms.tcc.oasis.controller.rdc216.Rdc216ManipuladorActivity;
-import com.admms.tcc.oasis.controller.rdc216.Rdc216PreparoActivity;
-import com.admms.tcc.oasis.controller.rdc216.Rdc216ArmazenamentoActivity;
-import com.admms.tcc.oasis.controller.rdc216.Rdc216HigieneActivity;
-import com.admms.tcc.oasis.controller.rdc216.Rdc216ResiduosActivity;
-import com.admms.tcc.oasis.controller.rdc216.Rdc216ResponsavelActivity;
-import com.admms.tcc.oasis.controller.rdc216.Rdc216SaneamentoActivity;
-import com.admms.tcc.oasis.controller.rdc216.Rdc216VetoresActivity;
+
+
+
 
 public class Prt2619Activity extends Activity {
 
@@ -26,23 +18,23 @@ public class Prt2619Activity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prt2619);
 
-        ImageButton armazenamento = (ImageButton) findViewById(R.id.iten_armazenamento);
-        ImageButton documentacao = (ImageButton) findViewById(R.id.iten_documentacao);
-        ImageButton edificacao = (ImageButton) findViewById(R.id.iten_edificacao);
-        ImageButton higiene = (ImageButton) findViewById(R.id.iten_higiene);
-        ImageButton ingredientes = (ImageButton) findViewById(R.id.iten_ingredientes);
-        ImageButton manipulador = (ImageButton) findViewById(R.id.iten_manipulador);
-        ImageButton vetores = (ImageButton) findViewById(R.id.iten_vetores);
-        ImageButton preparo = (ImageButton) findViewById(R.id.iten_preparo);
-        ImageButton residuos = (ImageButton) findViewById(R.id.iten_residuos);
-        ImageButton responsavel = (ImageButton) findViewById(R.id.iten_responsavel);
-        ImageButton saneamento = (ImageButton) findViewById(R.id.iten_saneamento);
+        ImageButton armazenamento = (ImageButton) findViewById(R.id.prt2619_armazenamento_prt2619);
+        ImageButton documentacao = (ImageButton) findViewById(R.id.prt2619_documentacao_prt2619);
+        ImageButton edificacao = (ImageButton) findViewById(R.id.prt2619_edificacao_prt2619);
+        ImageButton exposicao = (ImageButton) findViewById(R.id.prt2619_exposicao_prt2619);
+        ImageButton higiene = (ImageButton) findViewById(R.id.prt2619_higiene_prt2619);
+        ImageButton ingredientes = (ImageButton) findViewById(R.id.prt2619_ingredientes_prt2619);
+        ImageButton manipulador = (ImageButton) findViewById(R.id.prt2619_manipulador_prt2619);
+        ImageButton vetores = (ImageButton) findViewById(R.id.prt2619_vetores_prt2619);
+        ImageButton preparo = (ImageButton) findViewById(R.id.prt2619_preparo_prt2619);
+        ImageButton residuos = (ImageButton) findViewById(R.id.prt2619_residuos_prt2619);
+        ImageButton saneamento = (ImageButton) findViewById(R.id.prt2619_saneamento_prt2619);
 
 
         armazenamento.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intentVaiProArmazenamento = new Intent(Prt2619Activity.this, Rdc216ArmazenamentoActivity.class);
+                Intent intentVaiProArmazenamento = new Intent(Prt2619Activity.this, Prt2619_ArmazenamentoActivity.class);
                 startActivity(intentVaiProArmazenamento);
             }
         });
@@ -50,7 +42,7 @@ public class Prt2619Activity extends Activity {
         documentacao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intentVaiPraDocumentacao = new Intent(Prt2619Activity.this, Rdc216DocumentacaoActivity.class);
+                Intent intentVaiPraDocumentacao = new Intent(Prt2619Activity.this, Prt2619_DocumentacaoActivity.class);
                 startActivity(intentVaiPraDocumentacao);
             }
         });
@@ -58,66 +50,67 @@ public class Prt2619Activity extends Activity {
         edificacao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intentVaiPraEdificacao = new Intent(Prt2619Activity.this, Rdc216EdificacaoActivity.class);
+                Intent intentVaiPraEdificacao = new Intent(Prt2619Activity.this, Prt2619_EdificacaoActivity.class);
                 startActivity(intentVaiPraEdificacao);
             }
         });
+
+        exposicao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentVaiPraExposicao = new Intent(Prt2619Activity.this, Prt2619_ExposicaoActivity.class);
+                startActivity(intentVaiPraExposicao);
+            }
+        });
+
         higiene.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intentVaiPraHigiene = new Intent(Prt2619Activity.this, Rdc216HigieneActivity.class);
+                Intent intentVaiPraHigiene = new Intent(Prt2619Activity.this, Prt2619_HigieneActivity.class);
                 startActivity(intentVaiPraHigiene);
             }
         });
         ingredientes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intentVaiPraIngredientes = new Intent(Prt2619Activity.this, Rdc216IngredientesActivity.class);
+                Intent intentVaiPraIngredientes = new Intent(Prt2619Activity.this, Prt2619_IngredientesActivity.class);
                 startActivity(intentVaiPraIngredientes);
             }
         });
         manipulador.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intentVaiPraManipulador = new Intent(Prt2619Activity.this, Rdc216ManipuladorActivity.class);
+                Intent intentVaiPraManipulador = new Intent(Prt2619Activity.this, Prt2619_ManipuladoresActivity.class);
                 startActivity(intentVaiPraManipulador);
             }
         });
         vetores.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intentVaiPraVetores = new Intent(Prt2619Activity.this, Rdc216VetoresActivity.class);
+                Intent intentVaiPraVetores = new Intent(Prt2619Activity.this, Prt2619_VetoresActivity.class);
                 startActivity(intentVaiPraVetores);
             }
         });
         preparo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intentVaiPraPreparo = new Intent(Prt2619Activity.this, Rdc216PreparoActivity.class);
+                Intent intentVaiPraPreparo = new Intent(Prt2619Activity.this, Prt2619_PreparoActivity.class);
                 startActivity(intentVaiPraPreparo);
             }
         });
         residuos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intentVaiPraResiduos = new Intent(Prt2619Activity.this, Rdc216ResiduosActivity.class);
+                Intent intentVaiPraResiduos = new Intent(Prt2619Activity.this, Prt2619_ResiduosActivity.class);
                 startActivity(intentVaiPraResiduos);
-            }
-        });
-        responsavel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intentVaiPraResponsavel = new Intent(Prt2619Activity.this, Rdc216ResponsavelActivity.class);
-                startActivity(intentVaiPraResponsavel);
             }
         });
         saneamento.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intentVaiPraSaneamento = new Intent(Prt2619Activity.this, Rdc216SaneamentoActivity.class);
+                Intent intentVaiPraSaneamento = new Intent(Prt2619Activity.this, Prt2619_SaneamentoActivity.class);
                 startActivity(intentVaiPraSaneamento);
             }
         });
-
     }
 }
